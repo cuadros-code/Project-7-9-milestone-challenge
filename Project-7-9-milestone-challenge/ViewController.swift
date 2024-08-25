9cac14debc0e3184e4aeed54b096f8e84465a676
kevin cuadros <kevindavidcuadros@gmail.com>
2024-08-25 18:02:27 -0500

    add keyboard

--- Project-7-9-milestone-challenge/ViewController.swift
+++ Project-7-9-milestone-challenge/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var listWords = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor(red: 0.00, green: 0.30, blue: 0.81, alpha: 1.00)
+        
+        if let fileULR = Bundle.main.url(forResource: "list", withExtension: "txt") {
+            if let fileContent = try? String(contentsOf: fileULR){
+                
+            }
+        }
     }
-
-
+    
+    @IBAction func buttonTap(_ sender: UIButton) {
+        print(sender.titleLabel?.text! ?? "")
+    }
 }
 
