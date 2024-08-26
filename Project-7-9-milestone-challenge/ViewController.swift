58d4dd2a670548b9e83f525a705450d51922c520
kevin cuadros <kevindavidcuadros@gmail.com>
2024-08-25 22:10:45 -0500

    Add animation failed guess and NotificartionFeedbackGenerator

--- Project-7-9-milestone-challenge/ViewController.swift
+++ Project-7-9-milestone-challenge/ViewController.swift
@@ -7,23 +7,72 @@
 
 import UIKit
 
+let generator = UINotificationFeedbackGenerator()
+
 class ViewController: UIViewController {
+    @IBOutlet weak var primaryWord: UILabel!
+    @IBOutlet weak var wordCount: UILabel!    
     
     var listWords = [String]()
+    var selectedWordReferencie: String = ""
+    var selectedWord: String = "" {
+        didSet {
+            wordCount.text = "\(selectedWord.count) letters"
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor(red: 0.00, green: 0.30, blue: 0.81, alpha: 1.00)
         
         if let fileULR = Bundle.main.url(forResource: "list", withExtension: "txt") {
             if let fileContent = try? String(contentsOf: fileULR){
+                listWords = fileContent.components(separatedBy: "\n")
+                listWords.shuffle()
+                listWords.removeAll(where: { $0.isEmpty })
+                selectedWord = listWords[0]
+                for _ in selectedWord {
+                    selectedWordReferencie += "?"
+                }
+                primaryWord.text = selectedWordReferencie
+                print(selectedWord)
+                print(selectedWordReferencie)
                 
             }
         }
     }
     
     @IBAction func buttonTap(_ sender: UIButton) {
-        print(sender.titleLabel?.text! ?? "")
+        guard let buttonText = sender.titleLabel?.text else { return }
+
+        var matchFound = false
+
+        for (index, letter) in selectedWord.enumerated() {
+            if String(letter) == buttonText {
+                if let stringIndex = selectedWordReferencie.index(
+                    selectedWordReferencie.startIndex,
+                    offsetBy: index,
+                    limitedBy: selectedWordReferencie.endIndex
+                ) {
+                    selectedWordReferencie.replaceSubrange(stringIndex...stringIndex, with: buttonText)
+                    matchFound = true
+                }
+            }
+        }
+
+        if matchFound {
+            primaryWord.text = selectedWordReferencie
+        } else {
+            let animation = CABasicAnimation(keyPath: "position")
+            animation.duration = 0.06
+            animation.repeatCount = 2
+            animation.autoreverses = true
+            animation.fromValue = NSValue(cgPoint: CGPoint(x: primaryWord.center.x - 10, y: primaryWord.center.y))
+            animation.toValue = NSValue(cgPoint: CGPoint(x: primaryWord.center.x + 10, y: primaryWord.center.y))
+            primaryWord.layer.add(animation, forKey: "position")
+            generator.notificationOccurred(.error)
+        }
     }
+    
+    
 }
 
